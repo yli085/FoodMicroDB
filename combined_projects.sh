eba9e1e747cd7ce154c962eae5e5f2bb2a9ff729
Yahui Li <61255999+yli085@users.noreply.github.com>
2024-07-23 11:09:49 +0800

    Delete combined_projects.sh

--- combined_projects.sh
+++
@@ -1,4 +0,0 @@
-# shell
-for i in p c o f g; do
-  cp /Users/yahui/Desktop/project_data/data/project_16S/temp/${i}/* /Users/yahui/Documents/01work/09Liu_lab/02FoodMicroDB/0add_data/YLv3/newdata0620/all_amplicon_results/16S/temp/${i}
-done
